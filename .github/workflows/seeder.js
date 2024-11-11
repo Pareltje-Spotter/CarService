ab59b087dfd8e2a2565c56d09b2c75814852e3af
Niels <Nielsfeijen76@gmail.com>
2024-11-11 14:36:22 +0100

    seeder

---
+++ .github/workflows/seeder.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb://mongoadmin:mongoadmin@mongo:27018";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true   
+ });
+
+async function seedDatabase() {
+  try {
+    await client.connect();
+    const database = client.db("carInfo");
+    const collection = database.collection("info");
+
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.licenseplate = licenseplate;
+
+    const data = [
+      { brand: "HONDA", model: "CIVIC", year: 2002, licenseplate: "TH926F"},
+      { brand: "BMW", model: "M4", year: 2012, licenseplate: "XJKK03"},
+    ];
+
+    const result = await collection.insertMany(data);
+    console.log(`${result.insertedCount} documents were inserted`);
+  } catch (err) {
+    console.error(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+seedDatabase().catch(console.dir);
