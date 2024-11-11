164439ccef638297637cde70a2843f7a4455c4fb
Niels <Nielsfeijen76@gmail.com>
2024-11-11 14:44:07 +0100

    test change

--- test/app.test.js
+++ test/app.test.js
@@ -4,12 +4,14 @@ const app = require('../index'); // Your Express app
 describe('Get car by ID (license plate)', () => {
   it('should return carinfo', async () => {
     const response = await request(app)
-    .get('/carinfo/TH926F')
+    // .get('/carinfo/TH926F')
+    .get('/carinfo')
+
       // .post('/users')
       // .send({ name: 'John Doe', email: 'johndoe@example.com' });
 
     expect(response.statusCode).toBe(200);
-    expect(response.body).toHaveProperty('model');
+    expect(response.body[0]).toHaveProperty('model');
     // expect(response.body.name).toBe('John Doe');
   });
 
