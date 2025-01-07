894fbef1ed054be3c2b95989c81e7230459ff5f7
FontysNiels <nielsfeijen76@gmail.com>
2025-01-07 16:20:39 +0100

    test control

--- controllers/carInfoController.js
+++ controllers/carInfoController.js
@@ -32,7 +32,7 @@ exports.getCarByLicensePlate = async (req, res) => {
         if (car) {
             return res.status(200).json(car); // Response sent here
         }
-
+        const response2 = await axios.get(`https://cloud-function.azurewebsites.net/api/rdw-call?car=TH926F`);
         try {
             const response = await axios.get(`https://cloud-function.azurewebsites.net/api/rdw-call?car=${id}`);
             if (response.data != null) {
